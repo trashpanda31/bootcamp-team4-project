fe8b527389f8546c01e094e5460e364ddcca81d9
trashpanda31 <el.rozenbergs@gmail.com>
2026-02-10 17:45:48 +0000

    add Cloudwatch

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -31,3 +31,20 @@ resource "aws_instance" "jenkins_team4" {
     Name = "CloudSprint-Jenkins-team4"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "team4_high_cpu" {
+  alarm_name          = "CloudSprint-team4-HighCPU"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 70
+
+  dimensions = {
+    InstanceId = var.instance_id
+  }
+
+  treat_missing_data = "missing"
+}
