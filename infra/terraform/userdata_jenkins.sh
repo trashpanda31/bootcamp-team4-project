5da5e350f52f5fdc88d9770f2b5b3f266c7cebd5
trashpanda31 <el.rozenbergs@gmail.com>
2026-02-10 14:59:34 +0000

    Update jenkins userdata

--- infra/terraform/userdata_jenkins.sh
+++ infra/terraform/userdata_jenkins.sh
@@ -13,8 +13,18 @@ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.
 
 sudo apt update -y
 sudo apt-get install -y jenkins
+sudo apt install -y  docker.io
+sudo apt install -y docker-compose
 
 echo 'JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64' | sudo tee -a /etc/default/jenkins > /dev/null
 
+sudo python3 -m venv /opt/ansible-venv
+sudo /opt/ansible-venv/bin/pip install --upgrade pip
+sudo /opt/ansible-venv/bin/pip install ansible
+/opt/ansible-venv/bin/ansible --version
+
+sudo chmod -R a+rX /opt/ansible-venv
+
 sudo systemctl enable --now jenkins
 sudo systemctl status jenkins --no-pager || true
+sudo systemctl enable --now  docker
