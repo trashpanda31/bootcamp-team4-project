cb788056a6d19a8441181deff416952f0a6559f1
JanisZelcs <janze2004@inbox.lv>
2026-02-11 12:50:42 +0200

    Add AWS Secrets Manager access for RDS

--- infra/terraform/userdata_docker.sh
+++ infra/terraform/userdata_docker.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y  docker.io 
-sudo apt install -y docker-compose
+sudo apt install -y docker.io docker-compose jq
 
 sudo systemctl enable docker
+sudo systemctl start docker
 
-docker --version
-docker compose version
+SECRET=$(aws secretsmanager get-secret-value --secret-id team4/wordpress/rds --query SecretString --output text)
+DB_USER=$(echo $SECRET | jq -r .username)
+DB_PASS=$(echo $SECRET | jq -r .password)
+DB_HOST=$(echo $SECRET | jq -r .host)
+DB_NAME="cloudsprint_team4"
+
+sudo docker run -d \
+  -e WORDPRESS_DB_HOST=$DB_HOST \
+  -e WORDPRESS_DB_USER=$DB_USER \
+  -e WORDPRESS_DB_PASSWORD=$DB_PASS \
+  -e WORDPRESS_DB_NAME=$DB_NAME \
+  -p 80:80 \
+  wordpress:latest
